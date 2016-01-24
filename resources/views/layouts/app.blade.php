b8d759964a438344864715088e9d87fab23b48e0
Hector Peredo <hectorda@hotmail.es>
2016-01-24 00:31:21 -0300

    App Blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,11 +22,6 @@
 
 </head>
 <body id="app-layout">
-<div class="progress">
-  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-    <span class="sr-only">60% Complete</span>
-  </div>
-</div>
     @include('layouts.navbar-top')
     @yield('content')
 

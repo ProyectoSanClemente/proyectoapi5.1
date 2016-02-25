c9a168b1ff596580450430de290d3859c77c6d21
hectorda <hectorda@hotmail.es>
2016-02-25 12:35:00 -0300

    Test autologin

--- resources/views/cuentas/solicitudcompras.blade.php
+++ resources/views/cuentas/solicitudcompras.blade.php
@@ -4,14 +4,38 @@
 <div class="container">
 
     @include('common.errors')
-		<body onload="document.form1.submit()">
-		<!-- Modelo Impresora Field -->
-    {!! Form::open(['url' => ['http://reset.sanclemente.cl/sistemas/orden/index.php'],'method' => 'post','name'=>"form1"]) !!}
-		<br><br><br><br><br><br>
+		<form method="post" id="solicitudcompras" name="form1" class="KT_tngformerror" action="http://reset.sanclemente.cl/sistemas/orden/index.php" style="display:none">
+         <em><img src="images/Logo-solicitud.jpg" width="297" height="58" alt="Solicitud de Compra"></em>
+         <table cellpadding="2" cellspacing="0" class="KT_tngtable" kt_checkboxes_attached="true" kt_uni_attached="true">
+          <tbody><tr>
+            <td class="KT_th"><label for="kt_login_user">Usuario:<span classname="KT_required" class="KT_required">*</span></label></td>
+            <td><div align="left">
+              <input type="text" name="kt_login_user" id="kt_login_user" value="{{$user}}" size="25">
+                </div></td>
+          </tr>
+          <tr>
+            <td class="KT_th"><label for="kt_login_password">Contraseña:<span classname="KT_required" class="KT_required">*</span></label></td>
+            <td><div align="left">
+              <input type="password" name="kt_login_password" id="kt_login_password" value="{{$pass}}" size="25">
+                </div></td>
+          </tr>
+          <tr class="KT_buttons">
+            <td colspan="2"><input type="submit" name="kt_login1" id="kt_login1" value="Entrar">            </td>
+          </tr>
+        </tbody></table>
+      </form>
+	</div>
+
+
 		<div align="center">{!!HTML::image("images/load/default.gif")!!}
 			Redireccionando, por favor espere...
 		</div>
-		{!!Form::hidden('kt_login_user',$user)!!}
-    {!! Form::close() !!}
-</div>
+
+@push('scripts')
+<script type="text/javascript">
+$( document ).ready(function() {
+	$('#kt_login1').click();
+});
+
+</script>
 @endsection
--- resources/views/cuentas/zimbra.blade.php
+++ resources/views/cuentas/zimbra.blade.php
@@ -4,14 +4,42 @@
 <div class="container">
 
     @include('common.errors')
-
 		<!-- Modelo Impresora Field -->
-				<body onload="document.form1.submit()">
-    {!! Form::open(['url' => ['https://www.sanclemente.cl/?username='.$user],'method' => 'post','id'=>'form1','name'=>'form1']) !!}
-    <br><br><br><br><br><br>
-    <div align="center">{!!HTML::image("images/load/default.gif")!!}
-	Redireccionando, por favor espere...
+
+
+		{!! Form::open(['url' => ['https://www.sanclemente.cl'],'method' => 'post','id'=>'zimbra','name'=>'loginForm','style'=>"display:none","accept-charset"=>"UTF-8"]) !!}
+
+                <input type="hidden" name="loginOp" value="login">                 
+				
+					
+				<label for="username">Nombre de usuario:</label></td>
+                
+                <input id="username" class="zLoginField" name="username" type="text" value="{{$user}}" size="40" maxlength="1024" autocapitalize="off" autocorrect="off">
+                
+                <label for="password">Contraseña:</label>
+                
+                <input id="password" autocomplete="off" class="zLoginField" name="password" type="password" value="{{$pass}}"size="40" maxlength="1024">
+
+
+                    <input id="remember" value="1" type="checkbox" name="zrememberme">
+                    <label for="remember">Recordarme</label>
+                    <input type="submit" class="ZLoginButton DwtButton" value="Iniciar sesión">
+ 
+			{!! Form::close()!!}
+
+	<div align="center">{!!HTML::image("images/load/default.gif")!!}
+		Redireccionando, por favor espere...
 	</div>
-    {!! Form::close() !!}
+
 </div>
 @endsection
+
+@push('scripts')
+<script type="text/javascript">
+$( document ).ready(function() {
+
+    $('form#zimbra').submit();
+});
+
+</script>
+@endpush
